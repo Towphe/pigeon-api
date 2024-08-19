79b2090f043f4bb1f72b03bf53f45b5e17a9d036
JC Castillo <castillochristopherjames@gmail.com>
2024-08-19 23:28:52 +0800

    created Supabase-compatible Authorization filter

---
+++ src/services/auth/SupabaseAuthorize.cs
@@ -0,0 +1,24 @@
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace services.auth;
+
+// for some reason, [Authorize] does not work for Supabase.
+public class SupabaseAuthorize : Attribute, IAsyncAuthorizationFilter
+{
+    public SupabaseAuthorize()
+    {
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var userId = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        if (userId == null)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
